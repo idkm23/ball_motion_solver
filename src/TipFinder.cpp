3bdfa88d2c0ecb0dd5674e9a8e9aae0fdcc2b134
ylmeng <yuanliang.meng@gmail.com>
2015-11-02 10:44:39 -0500

    Added hand detection and position prediction

--- src/TipFinder.cpp
+++ src/TipFinder.cpp
@@ -3,6 +3,7 @@
 using namespace cv;
 
 const int TipFinder::NOT_FOUND_COORD = -1337;
+const bool TipFinder::FULL_DETECTION = true;
 
 //warp perspective matrix
 const Mat TipFinder::H = 
@@ -19,7 +20,10 @@ TipFinder::TipFinder() {
     img_sub = n.subscribe<sensor_msgs::Image>
         ("/ball_mover/camera/image"/*"/usb_cam/image_raw"*/, 10, &TipFinder::img_callback, this);
     tip_point_pub = n.advertise<geometry_msgs::Point>("/ball_mover/tip_point", 1000);
+    n_missing_pub = n.advertise<std_msgs::Int32>("/ball_mover/n_missing", 1000);
 
+    n_missing.data = 0;
+    
     /* webcam-friendly hsv values 
     iLowH = 39;
     iHighH = 81;
@@ -38,57 +42,87 @@ TipFinder::TipFinder() {
     iLowV = 78;
     iHighV = 255;
     
-
-    namedWindow("ControlTip", CV_WINDOW_AUTOSIZE); //create a window called "Control"
-
-    //Create trackbars in "Control" window to dynamically adjust HSV threshold values
-    cvCreateTrackbar("LowH", "Control", &iLowH_tip, 179); //Hue (0 - 179)
-    cvCreateTrackbar("HighH", "Control", &iHighH_tip, 179);
-
-    cvCreateTrackbar("LowS", "Control", &iLowS_tip, 255); //Saturation (0 - 255)
-    cvCreateTrackbar("HighS", "Control", &iHighS_tip, 255);
-
-    cvCreateTrackbar("LowV", "Control", &iLowV_tip, 255); //Value (0 - 255)
-    cvCreateTrackbar("HighV", "Control", &iHighV_tip, 255);
-
-}
-
-TipFinder::HandFinder() {
-
-    img_sub = n.subscribe<sensor_msgs::Image>
-        ("/ball_mover/camera/image"/*"/usb_cam/image_raw"*/, 10, &TipFinder::img_callback, this);
-    hand_point_pub = n.advertise<geometry_msgs::Point>("/ball_mover/hand_point", 1000);
-
-   
-    /* webcam-friendly hsv values 
-    iLowH = 0;
-    iHighH = 65; 
-    iLowS = 46;
-    iHighS = 213;
-    iLowV = 124;
-    iHighV = 141;
-    */ 
-
-    /* glass-friendly hsv values */ 
+    /* glass-friendly hsv values for hand*/ 
     iLowH_hand = 0;
     iHighH_hand = 43;
     iLowS_hand = 46;
     iHighS_hand = 106;
     iLowV_hand = 20;
-    iHighV_hand = 255; 
+    iHighV_hand = 255;
 
-    namedWindow("ControlHand", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+    namedWindow("ControlTip", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
     //Create trackbars in "Control" window to dynamically adjust HSV threshold values
-    cvCreateTrackbar("LowH", "Control", &iLowH_hand, 179); //Hue (0 - 179)
-    cvCreateTrackbar("HighH", "Control", &iHighH_hand, 179);
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
 
-    cvCreateTrackbar("LowS", "Control", &iLowS_hand, 255); //Saturation (0 - 255)
-    cvCreateTrackbar("HighS", "Control", &iHighS_hand, 255);
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
-    cvCreateTrackbar("LowV", "Control", &iLowV_hand, 255); //Value (0 - 255)
-    cvCreateTrackbar("HighV", "Control", &iHighV_hand, 255);
+    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
+}
+
+geometry_msgs::Point TipFinder::get_centroid(Mat& imgThresholded, int areaThreshold) {
+
+    geometry_msgs::Point point;
+    point.x = NOT_FOUND_COORD;
+    point.y = NOT_FOUND_COORD;
+    point.z = NOT_FOUND_COORD;
+
+    //morphological opening (remove small objects from the foreground)
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+    //morphological closing (fill small holes in the foreground)
+    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+    Moments oMoments = moments(imgThresholded);
+
+    double dM01 = oMoments.m01;
+    double dM10 = oMoments.m10;
+    double dArea = oMoments.m00;
+
+    //if the colored mass is large enough, then...
+    if (dArea > areaThreshold) {
+        //finds the center of the tip within the raw perspective
+        int raw_x = dM10 / dArea;
+        int raw_y = dM01 / dArea;
+       
+        Mat raw_point = (Mat_<double>(3, 1) << raw_x, raw_y, 1);
+        
+        //transform point with H matrix
+        Mat warped_point = H*raw_point;        
+        int warped_x = warped_point.at<double>(0, 0) / warped_point.at<double>(2, 0);
+        int warped_y = warped_point.at<double>(1, 0) / warped_point.at<double>(2, 0);        
+
+        //draws the center of the colored mass in binary image 
+        cvtColor(imgThresholded, imgThresholded, COLOR_GRAY2BGR);
+        ellipse(imgThresholded, Point(raw_x, raw_y), Size(10, 10), 360, 0, 360, Scalar(100, 100, 255), 5, 8);
+        
+        point.x = warped_x;
+        point.y = warped_y;
+        point.z = 1;
+    return point;
+    }
+}
+
+geometry_msgs::Point TipFinder::get_velocity() {
+    geometry_msgs::Point velocity, predicted;
+    predicted.x = NOT_FOUND_COORD;
+    predicted.y = NOT_FOUND_COORD;
+
+    if (point_tracer.size()<=1) return predicted;
+    // otherwise, compute the average velocity, pixels/frame
+    velocity.x = std::abs ( (point_tracer.back().x - point_tracer.front().x) )
+                            / (float)(point_tracer.size() - 1);
+    velocity.y = std::abs ( (point_tracer.back().y - point_tracer.front().y) )
+                            / (float)(point_tracer.size() - 1);
+    
+   
+    return velocity;
 }
 
 /**
@@ -116,51 +150,60 @@ void TipFinder::img_callback(const sensor_msgs::ImageConstPtr& ros_img)
     cvtColor(raw_scene, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
     Mat imgThresholded;
-
     inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
     //Threshold the image
-
-    //morphological opening (remove small objects from the foreground)
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-    //morphological closing (fill small holes in the foreground)
-    dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-    erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-
-    Moments oMoments = moments(imgThresholded);
-
-    double dM01 = oMoments.m01;
-    double dM10 = oMoments.m10;
-    double dArea = oMoments.m00;
     
-    geometry_msgs::Point tip_point;
-    tip_point.x = NOT_FOUND_COORD;
-    tip_point.y = NOT_FOUND_COORD;
-    tip_point.z = NOT_FOUND_COORD;
+    geometry_msgs::Point tip_point = get_centroid(imgThresholded, 6000);
 
-    //if the colored mass is large enough, then...
-    if (dArea > 6000) {
-        //finds the center of the tip within the raw perspective
-        int raw_x = dM10 / dArea;
-        int raw_y = dM01 / dArea;
-       
-        Mat raw_point = (Mat_<double>(3, 1) << raw_x, raw_y, 1);
-        
-        //transform point with H matrix
-        Mat warped_point = H*raw_point;        
-        int warped_x = warped_point.at<double>(0, 0) / warped_point.at<double>(2, 0);
-        int warped_y = warped_point.at<double>(1, 0) / warped_point.at<double>(2, 0);        
+    bool reliable_detection = false;
+    if (tip_point.x>-1 && tip_point.y>-1) 
+        reliable_detection = true;
 
-        //draws the center of the colored mass in binary image 
-        cvtColor(imgThresholded, imgThresholded, COLOR_GRAY2BGR);
-        ellipse(imgThresholded, Point(raw_x, raw_y), Size(10, 10), 360, 0, 360, Scalar(100, 100, 255), 5, 8);
+/*
+    if (tip_point.x>-1 && tip_point.y>-1){
+        reliable_detection = true;
+
+        if (point_tracer.size() >=5) && n_missing.data<5){
+            geometry_msgs::Point previous_velocity = get_velocity();
+            if ( std::abs(tip_point.x-point_tracer.back().x) > 300*previous_velocity.x
+                || std::abs(tip_point.y-point_tracer.back().y) > 300*previous_velocity.y) {
+                reliable_detection = false;
+                tip_point.x = NOT_FOUND_COORD;
+                tip_point.y = NOT_FOUND_COORD;
+            }
+        }
+    }
+*/
+    if (reliable_detection)
+        n_missing.data = 0; // reset missing counter
+    else
+        n_missing.data += 1;
+
+    if (FULL_DETECTION && ! reliable_detection ) {
+        /* perform hand dectection */
+        Mat imgThresholded;
+
+        inRange(imgHSV, Scalar(iLowH_hand, iLowS_hand, iLowV_hand), Scalar(iHighH_hand, iHighS_hand, iHighV_hand), imgThresholded);
+        //Threshold the image
+
+        geometry_msgs::Point hand_point = get_centroid(imgThresholded, 62000);
         
-        tip_point.x = warped_x;
-        tip_point.y = warped_y;
-    } 
+        // Set hand point as tip point for now
+        tip_point = hand_point;
+    }
     
+/*
+    if (tip_point.x>-1 && tip_point.y>-1) 
+        point_tracer.push(tip_point);
+    
+    if (point_tracer.size()>5)
+        point_tracer.pop();
+*/
+
     tip_point_pub.publish(tip_point);
+
+    // for debug
+    n_missing_pub.publish(n_missing);
     
     imshow("camera", imgThresholded);
     imshow("camera_raw", raw_scene);
--- src/TipFinder.h
+++ src/TipFinder.h
@@ -6,6 +6,7 @@
 //image transport/conversion libraries
 #include <sensor_msgs/Image.h>
 #include <geometry_msgs/Point.h>
+#include <std_msgs/Int32.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
@@ -13,6 +14,8 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
+
+#include <queue>
 
 /**
  * Finds the tip in the camera's context using color detection
@@ -22,15 +25,15 @@ class TipFinder {
 
 private:
     ros::NodeHandle n;
-    
+
     /* updates the ball's location based upon any object detection */
-    ros::Publisher tip_point_pub;
+    ros::Publisher tip_point_pub, n_missing_pub;
     
     /* fetches the scene to detect the image */
     ros::Subscriber img_sub; 
 
     /* Thresholding values for color detection */
-    int iLowH_tip, iHighH_tip, iLowS_tip, iHighS_tip, iLowV_tip, iHighV_tip;
+    int iLowH, iHighH, iLowS, iHighS, iLowV, iHighV;
     int iLowH_hand, iHighH_hand, iLowS_hand, iHighS_hand, iLowV_hand, iHighV_hand;
 
     /* warp perspective matrix */
@@ -38,6 +41,15 @@ private:
 
     /* Coordinate constant to express when the box is not found */
     static const int NOT_FOUND_COORD;
+
+    /* Use other methods to find target */
+    static const bool FULL_DETECTION;
+
+    /* a queue to save previous positions, may be used to predict curret position.*/
+    std::queue<geometry_msgs::Point> point_tracer;
+
+    /* number of missing detections */
+    std_msgs::Int32 n_missing;
 
 public:
     
@@ -51,6 +63,14 @@ public:
      * @param ros_img, image of object in a ros msg
      */
     void img_callback(const sensor_msgs::ImageConstPtr&);
+    
+    /** compute centroid of a mass
+       takes a thresholded image, and a threshold value for area
+    */
+    geometry_msgs::Point get_centroid(cv::Mat&, int areaThreshold);
+
+    /* predict position */
+    geometry_msgs::Point get_velocity();
 };
 
 #endif
